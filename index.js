350f38518ca819a6c47fa6688857a0a8389e6b73
Michael Harris <xero5k@yahoo.com>
2021-07-21 21:26:09 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 62, 
+    num2 = 1, 
+    num3 = 16, 
+    num4 = 6;
+
+const multiply = num1 * num2,
+    random = Math.ceil(Math.random()),
+    mod = num3 % num4,
+    max = Math.max(1,2,4,5,20);
